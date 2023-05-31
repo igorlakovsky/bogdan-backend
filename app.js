b88b480ffbdf01029f2be35365e9c92250b3826f
Igor Lakovsky <igorlakovsky@yandex.ru>
2023-05-31 10:37:10 +0300

    init server

---
+++ app.js
@@ -0,0 +1,16 @@
+require("dotenv").config();
+
+const express = require("express");
+
+const app = express();
+const port = process.env.SERVER_PORT;
+
+app.use(express.static("public"));
+
+app.get("/", (req, res) => {
+  res.send("Hello World!");
+});
+
+app.listen(port, () => {
+  console.log(`Example app listening on port ${port}`);
+});
